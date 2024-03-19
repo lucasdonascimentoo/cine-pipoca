494a701405fc6e4cf106b82f9bfc0d2d9ccab327
Lucas do Nascimento <lucasdoncr07@hotmail.com>
2024-03-19 15:23:08 -0300

    imagens filmes e ajuste inicial slider

--- meu-projeto/src/components/pages/Filmes.js
+++ meu-projeto/src/components/pages/Filmes.js
@@ -1,17 +1,26 @@
 import {Swiper, SwiperSlide} from 'swiper/react'
 import {Autoplay} from 'swiper/modules'
-import aftersun from '../../img/aftersun.png'
-import her from '../../img/her.jpg'
-import lobo from '../../img/Lobo_atras.jpg'
-import pinoquio from '../../img/pinoquio.jpg'
-import portal from '../../img/portal.png'
+import aftersun from '../../img2/aftersun_capa.png'
+import argentina from '../../img2/argentina_capa.jpg'
+import che from '../../img2/che_capa.jpg'
+import entergalatic from '../../img2/entergalatic_capa.png'
+import greenbook from '../../img2/greenbook_capa.jpg'
+import her from '../../img2/her_capa.jpg'
+import inimigos from '../../img2/inimigos_capa.jpg'
+import licorice_pizza from '../../img2/licorice_pizza_capa.jpg'
+import lobo from '../../img2/lobo_capa.jpg'
+import marighella from '../../img2/marighella_capa.jpg'
+import pinoquio from '../../img2/pinoquio_capa.jpg'
+import portal from '../../img2/portal_capa.jpg'
+import queeneslim from '../../img2/queeneslim_capa.jpg'
+import quehoras from '../../img2/quehoras_capa.jpg'
 
 import styles from './FilmesSeries.module.css'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/autoplay'
 function Filmes(){
-    const slides = [aftersun, her, lobo, pinoquio, portal]
+    const slides = [aftersun, argentina, che, entergalatic, greenbook, her, inimigos, licorice_pizza,  lobo, marighella,  pinoquio, portal, queeneslim, quehoras]
  
     return(
         <div className={styles.filmes_series}>
--- meu-projeto/src/components/pages/Filmes1.js
+++ meu-projeto/src/components/pages/Filmes1.js
@@ -1,8 +1,17 @@
 import aftersun from '../../img/aftersun.png'
-import her from '../../img/her.jpg'
-import lobo from '../../img/Lobo_atras.jpg'
-import pinoquio from '../../img/pinoquio.jpg'
+import argentina from '../../img/argentina.png'
+import che from '../../img/che.png'
+import entergalactic from '../../img/entergalactic.png'
+import greenbook from '../../img/greenbook.png'
+import her from '../../img/her.png'
+import inimigos from '../../img/inimigos.png'
+import licorice_pizza from '../../img/licorice_pizza.png'
+import lobo from '../../img/lobo.png'
+import marighella from '../../img/marighella.png'
+import pinoquio from '../../img/pinoquio.png'
 import portal from '../../img/portal.png'
+import queeneslim from '../../img/queeneslim.png'
+import quehoras from '../../img/quehoras.png'
 
 import styles from  './FIlmes1.module.css'
 
@@ -10,10 +19,20 @@ function Filmes1(){
     return(
         <section className={styles.imagens}>
             <img src={aftersun}/>
+            <img src={argentina}/>
+            <img src={che}/>
+            <img src={entergalactic}/>
+            <img src={greenbook}/>
             <img src={her}/>
+            <img src={inimigos}/>
+            <img src={licorice_pizza}/>
             <img src={lobo}/>
+            <img src={marighella}/>
             <img src={pinoquio}/>
             <img src={portal}/>
+            <img src={queeneslim}/>
+            <img src={quehoras}/>
+            
         </section>
     )
 }
