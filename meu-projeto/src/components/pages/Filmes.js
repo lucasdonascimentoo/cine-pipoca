f88e4fb09df69e74151a67aa148e3fbf091980df
Rafael Tadeu <rafael.tadeu1313@gmail.com>
2024-03-29 15:21:56 -0300

    make the slider use only the favorite movies

--- meu-projeto/src/components/pages/Filmes.js
+++ meu-projeto/src/components/pages/Filmes.js
@@ -1,38 +1,65 @@
 import {Swiper, SwiperSlide} from 'swiper/react'
 import {Autoplay} from 'swiper/modules'
-import aftersun from '../../img2/aftersun_capa.png'
-import argentina from '../../img2/argentina_capa.jpg'
-import che from '../../img2/che_capa.jpg'
-import entergalatic from '../../img2/entergalatic_capa.png'
-import greenbook from '../../img2/greenbook_capa.jpg'
-import her from '../../img2/her_capa.jpg'
-import inimigos from '../../img2/inimigos_capa.jpg'
-import licorice_pizza from '../../img2/licorice_pizza_capa.jpg'
-import lobo from '../../img2/lobo_capa.jpg'
-import marighella from '../../img2/marighella_capa.jpg'
-import pinoquio from '../../img2/pinoquio_capa.jpg'
-import portal from '../../img2/portal_capa.jpg'
-import queeneslim from '../../img2/queeneslim_capa.jpg'
-import quehoras from '../../img2/quehoras_capa.jpg'
+import {useState, useEffect} from 'react'
 
 import styles from './FilmesSeries.module.css'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/autoplay'
+
 function Filmes(){
-    const slides = [aftersun, argentina, che, entergalatic, greenbook, her, inimigos, licorice_pizza,  lobo, marighella,  pinoquio, portal, queeneslim, quehoras]
- 
+    const [movies, setMoviesList] = useState([])
+    const [error, setError] = useState(null)
+
+    const FilmesFavoritos = ()=>{
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              // Replace the token below with your own!
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGQ4NmQ0NWNhYjY3ODFkZmVkMjRmMTI3MTYzMTMzYiIsInN1YiI6IjY1ZjlkY2MyNzc3NmYwMDE3ZDEyYjljNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i8WWFG1vQG8aBY7InH3tjfOH-uJn95yuAQnq4cdS9to'
+            }
+        };
+
+        fetch('https://api.themoviedb.org/3/account/21120431/favorite/movies?language=pt-BR&page=1&sort_by=created_at.asc',options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+            return response.json()
+        })
+        .then(response => setMoviesList(response.results))
+        .catch(error => setError(error.message))
+    }
+
+    useEffect(()=>{
+        FilmesFavoritos()
+    }, [])
+
     return(
         <div className={styles.filmes_series}>
-            <Swiper  
+            <Swiper
             autoplay={{delay: 2500}}
             modules={[Autoplay]}>
-                
-                {slides.map(slide => (
+                {!error && (
+                    <>
+                    {movies.map(slide => (
+                        <SwiperSlide key={slide.id}>
+                            <img src={`https://image.tmdb.org/t/p/w500${slide.poster_path}`} alt={slide.name}/>
+                        </SwiperSlide>
+                    ))}
+                    {!movies.length && (
+                        <SwiperSlide>
+                            <div className={styles.empty}>No movies found!</div>
+                        </SwiperSlide>
+                    )}
+                    </>
+                )}
+                {error && (
                     <SwiperSlide>
-                        <img src={slide} alt={slides}/>
+                        <div className={styles.error}>{error}</div>
                     </SwiperSlide>
-                ))}
+                )}
             </Swiper>
         </div>
     )
